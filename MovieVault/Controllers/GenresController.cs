1e3e901f1524dcce885168b0372bb6b76235b7df
Student <example@example.com>
2019-05-07 12:04:56 -0700

    add Movie class getters, Get+Find+ClearAll+Save functions

--- MovieVault/Controllers/GenresController.cs
+++ MovieVault/Controllers/GenresController.cs
@@ -8,8 +8,50 @@ namespace MovieVault.Controllers
   public class GenresController : Controller
   {
 
+    [HttpGet("/genres")]
+    public ActionResult Index()
+    {
+      List<Genre> allGenres = Genre.GetAll();
+      return View(allGenres);
+    }
 
+    [HttpGet("/genres/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
 
+    [HttpPost("/genres")]
+    public ActionResult Create(string genreName)
+    {
+      Genre newGenre = new Genre(genreName);
+      List<Genre> allGenres = Genre.GetAll();
+      return View("Index", allGenres);
+    }
 
-    
+    [HttpGet("/genre/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Genre selectedGenre = Genre.Find(id);
+      List<Movie> genreMovie = selectedGenre.GetItem();
+      model.Add("genre", selectedGenre);
+      model.Add("movies", genreMovie);
+      return View(model);
+    }
+
+    [HttpGet("/genres/{genreId}/movies")]
+    public ActionResult Create(int genreId. string movieDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Genre foundGenre = Genre.Find(GenreId);
+      Movies newMovie = new Movie(movieDescription);
+      newMovie.Save();
+      foundGenre.AddItem(newMovie);
+      List<Movie> genreMovie = foundGenre.GetMovie();
+      model.Add("movie", genreMovie);
+      model.Add("genre", foundGenre);
+      return View("Show", model);
+    }
   }
+}
--- MovieVault/Models/Movie.cs
+++ MovieVault/Models/Movie.cs
@@ -17,7 +17,138 @@ namespace MovieVault.Models
       _year = year;
       _id = id;
     }
-    
 
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public string GetDirector()
+    {
+      return _director;
+    }
+
+    public int GetYear()
+    {
+      return _year;
+    }
+
+    public int (GetId)
+    {
+      return _id;
+    }
+
+
+    public override bool Equals(System.Object otherMovie)
+    {
+      if (!(otherMovie is Movie))
+      {
+        return false;
+      }
+      else
+      {
+        Movie newMovie = (Movie) otherMovie;
+        bool idEquality = (this.GetId() == newMovie.GetId());
+        bool descriptionEquality = (this.GetDescription() == newMovie.GetDescription());
+        return (idEquality && descriptionEquality);
+      }
+    }
+
+    public static Movie Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Opn();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM 'movies' WHERE id = @thisId;";
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = id;
+      cmd.Parameters.Add(thisId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int movieId = 0;
+      string movieName = "";
+      string movieDirector = "";
+      int movieYear = 0;
+      while (rdr.Read())
+      {
+        movieId = rdr.GetInt32(0);
+        movieName = rdr.GetString(1);
+        movieDirector = rdr.GetString(2);
+        movieYear = rdr.GetInt32(3);
+      }
+      Movie foundMovie = new Movie(movieName, movieDirector, movieYear, movieId);
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return foundMovie;
+    }
+
+    public static List<Movie> GetAll()
+    {
+      List<Movie> allMovies = new List<Movie> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM movies;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        movieId = rdr.GetInt32(0);
+        movieName = rdr.GetString(1);
+        movieDirector = rdr.GetString(2);
+        movieYear = rdr.GetInt32(3);
+        Movie newMovie = new Movie(movieName, movieDirector, movieYear, movieId);
+        allMovies.Add(newMovie);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allMovies;
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO movies (name, director, year) VALUES (@MovieName, @MovieDirector, @MovieYear);";
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@MovieName";
+      name.Value = _name;
+      MySqlParameter director = new MySqlParameter();
+      director.ParameterName = "@MovieDirector";
+      director.Value = _director;
+      MySqlParameter year = new MySqlParameter();
+      year.ParameterName = "@MovieYear";
+      year.Value = _year;
+      cmd.Parameters.Add(name, director, year);
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if(conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static void ClearAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM movies";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if(conn !=null)
+      {
+        conn.Dispose();
+      }
+    }
   }
 }
